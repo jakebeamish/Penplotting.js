3579bfbe3eafd366121b5536fc6c3cf6f866fc53
Jake Beamish <jakebeamish@gmail.com>
2024-05-27 14:24:11 +0100

    Add doc for createSVG function

--- source/createSVG.js
+++ source/createSVG.js
@@ -1,3 +1,9 @@
+/**
+ * Create an SVG element with specified width and height
+ * @param {number} width 
+ * @param {number} height 
+ * @returns an SVG element
+ */
 export function createSVG(width = 100, height = 100) {
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
