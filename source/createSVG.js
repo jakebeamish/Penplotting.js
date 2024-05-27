b38b9944e11cb3fab6738f74a8524eea057d7bab
Jake Beamish <jakebeamish@gmail.com>
2024-05-27 14:11:23 +0100

    Implement createSVG function

---
+++ source/createSVG.js
@@ -0,0 +1,8 @@
+export function createSVG(width = 100, height = 100) {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    svg.setAttribute('width', width);
+    svg.setAttribute('height', height);
+    
+    return svg;
+}
