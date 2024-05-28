55a5da24ccc8cd4bc667c071c4f79ce6b9a9ffe0
Jake Beamish <jakebeamish@gmail.com>
2024-05-28 19:06:05 +0100

    Add some optional stuff

--- source/Sketch.js
+++ source/Sketch.js
@@ -1,11 +1,31 @@
 import { createSVG } from "./createSVG.js";
+import { addLineToSVG } from "./addLineToSVG.js";
 
 export class Sketch {
-    constructor(width, height, units) {
+    constructor(width, height, options) {
         this.width = width;
         this.height = height;
-        this.units = units;
+        this.units = options?.units || '';
+        this.backgroundColor = options?.backgroundColor || 'transparent';
         this.lines = [];
-        this.svg = createSVG(this.width, this.height, this.units);
+        this.svg = createSVG(this.width, this.height, {
+            units: this.units,
+            backgroundColor: this.backgroundColor
+        });
+    }
+
+    draw() {
+        this.addLinesToSVG();
+        this.appendSVG();
+    }
+
+    addLinesToSVG() {
+        for (let line of this.lines) {
+            addLineToSVG(this.svg, line.a.x, line.a.y, line.b.x, line.b.y, { stroke: 'black', strokeWidth: 0.1 });
+        }
+    }
+
+    appendSVG() {
+        document.body.appendChild(this.svg);
     }
 }
--- source/index.js
+++ source/index.js
@@ -1,11 +1,11 @@
-import { addLineToSVG } from "./addLineToSVG.js";
-import { createSVG } from "./createSVG.js";
 import { Vector } from "./Vector.js";
 import { Line } from "./Line.js";
 import { Sketch } from "./Sketch.js";
 
 
-let sketch = new Sketch(297, 210);
+let sketch = new Sketch(297, 210, {
+    units: 'mm'
+});
 
 let { width, height } = sketch;
 let centre = new Vector(width / 2, height / 2);
@@ -16,9 +16,6 @@ for (let i = 0; i < 200; i++) {
     sketch.lines.push(new Line(a, b));
 }
 
-for (let line of sketch.lines) {
-    addLineToSVG(sketch.svg, line.a.x, line.a.y, line.b.x, line.b.y, { stroke: 'blue', strokeWidth: 0.1 });
-}
+sketch.draw();
 
 
-document.body.appendChild(sketch.svg)
