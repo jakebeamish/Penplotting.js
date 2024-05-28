b99b07606a6410b2bdff7d7b4edc4bda966c74c2
Jake Beamish <jakebeamish@gmail.com>
2024-05-28 15:12:27 +0100

    Update demo sketch

--- source/index.js
+++ source/index.js
@@ -2,19 +2,22 @@ import { addLineToSVG } from "./addLineToSVG.js";
 import { createSVG } from "./createSVG.js";
 import { Vector } from "./Vector.js";
 
-let svg = createSVG(200, 200);
+let svg = createSVG(500, 700);
 let width = svg.getAttribute("width");
 let height = svg.getAttribute("height");
 
 
 let centre = new Vector(width / 2, height / 2);
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 10; i++) {
     let other = Vector.fromArray([Math.random() * width, Math.random() * height])
     let distance = centre.distance(other);
 
     console.log(distance, centre.magnitude(), other.magnitude())
-    if (distance > 100 || distance < 50) {
+    // if (distance > 100 || distance < 50) {
         addLineToSVG(svg, centre.x, centre.y, other.x, other.y);
-    }
+    // }
 }
+
+
+
 document.body.appendChild(svg)
