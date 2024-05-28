2a87e67212aca98c4d52b1bb9b6bf7d42615524d
Jake Beamish <jakebeamish@gmail.com>
2024-05-28 09:07:57 +0100

    Improve docs to reflect that createSVG and addLineToSVG work with SVGElement objects

--- source/addLineToSVG.js
+++ source/addLineToSVG.js
@@ -1,3 +1,12 @@
+/**
+ * 
+ * @param {SVGElement} svg 
+ * @param {number} x1 
+ * @param {number} y1 
+ * @param {number} x2 
+ * @param {number} y2 
+ * @param {number} stroke 
+ */
 export function addLineToSVG(svg, x1, y1, x2, y2, stroke = 'black') {
     const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
     line.setAttribute('x1', x1);
--- source/createSVG.js
+++ source/createSVG.js
@@ -2,7 +2,7 @@
  * Create an SVG element with specified width and height
  * @param {number} width 
  * @param {number} height 
- * @returns an SVG element
+ * @returns {SVGElement}
  */
 export function createSVG(width = 100, height = 100) {
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
