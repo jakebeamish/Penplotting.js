bd5fdd7a134384cf84dc0420a46bb4fbcbbd788f
Jake Beamish <jakebeamish@gmail.com>
2024-05-28 09:13:23 +0100

    AddLineToSVG returns the svg

--- source/addLineToSVG.js
+++ source/addLineToSVG.js
@@ -6,6 +6,7 @@
  * @param {number} x2 
  * @param {number} y2 
  * @param {number} stroke 
+ * @returns {SVGElement}
  */
 export function addLineToSVG(svg, x1, y1, x2, y2, stroke = 'black') {
     const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
