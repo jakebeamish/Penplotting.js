cc669edbb16e5ea3a40bc67ec7f7583a4016e911
Jake Beamish <jakebeamish@gmail.com>
2024-05-28 13:13:49 +0100

    Add 2d vector class with fromArray method

---
+++ source/Vector.js
@@ -0,0 +1,10 @@
+export class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static fromArray(array) {
+        return new Vector(array[0], array[1]);
+    }
+}
--- source/index.js
+++ source/index.js
@@ -1,8 +1,17 @@
 import { addLineToSVG } from "./addLineToSVG.js";
 import { createSVG } from "./createSVG.js";
+import { Vector } from "./Vector.js";
 
 let svg = createSVG(200, 200);
+let width = svg.getAttribute("width");
+let height = svg.getAttribute("height");
 
-addLineToSVG(svg, 0, 0, 200, 200)
 
+let centre = new Vector(width / 2, height / 2);
+for (let i = 0; i < 100; i++) {
+    let other = Vector.fromArray([Math.random() * width, Math.random() * height])
+
+    addLineToSVG(svg, centre.x, centre.y, other.x, other.y);
+
+}
 document.body.appendChild(svg)
