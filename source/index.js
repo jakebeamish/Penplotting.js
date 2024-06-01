5583e8e2e3962bf9f55850e213456289c54cec5a
Jake Beamish <jakebeamish@gmail.com>
2024-06-01 11:44:57 +0100

    Update template sketch

--- source/index.js
+++ source/index.js
@@ -3,44 +3,32 @@ import { Line } from "./Line.js";
 import { Sketch } from "./Sketch.js";
 import { randomInteger } from "./random.js";
 import { wrap } from "./wrap.js";
-
+import { deduplicateLines } from "./deduplicateLines.js";
 
 let sketch = new Sketch(210, 297, {
     units: 'mm',
-    backgroundColor: 'ivory'
+    backgroundColor: 'gainsboro'
 });
 
 let { width, height } = sketch;
+const margin = 0.1 * width;
 
-let rows = 7;
-let cols = 7;
+let verts = 500;
 
-let x = randomInteger(0, cols) + 0.5
-let y = randomInteger(0, rows) + 0.5
-let start = new Vector(x * width/cols, y * height/rows)
-for (let i = 0; i < 50; i++) {
-    console.log(x, y)
-    if (Math.random() < 0.45) {
-        x = wrap(x + randomInteger(-5, 5), 0, cols);
-    } else if (Math.random() < 0.9) {
-y = wrap(y + randomInteger(-5, 5), 0, rows);
-    } else {
-        x = wrap(x + 1, 0, cols);
-        y = wrap(y + 1, 0, rows);
-    }
-
-let end = new Vector(x * width/cols, y * height/rows)
-
-
-
-    
-
-
-sketch.lines.push(new Line(start, end))
-start = end;
+for (let i = 0; i < verts; i++) {
+    const x = (margin) + ((i + 0.5) / verts) * (width - (margin * 2))
+    sketch.lines.push(new Line({ x, y: margin }, { x, y: height - margin }))
 }
 
 
+for (let i = 0; i < 1; i++) {
+
+    let y1 = randomInteger(margin, height - margin);
+    let y2 = randomInteger(margin, height - margin);
+
+    for (let j = 0; j < 500; j++) {
+        sketch.lines.push(new Line({ x: margin, y: wrap(y1 + j, margin, height-margin) }, { x: width - margin, y: wrap(y2 + j/0.5, margin, height-margin)}))
+    }
+}
+
 sketch.draw();
-
-
