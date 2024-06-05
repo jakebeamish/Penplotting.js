2e411c434cffb3161b2ea7727dcf0c93ab9e637d
Jake Beamish <jakebeamish@gmail.com>
2024-06-05 01:01:38 +0100

    Prefer double quotes

--- source/createSVG.js
+++ source/createSVG.js
@@ -7,20 +7,20 @@
  * @param {number} [width=100]
  * @param {number} [height=100]
  * @param {Object} [options]
- * @param {string} [options.units = '']
- * @param {string} [options.backgroundColor = 'transparent']
+ * @param {string} [options.units = ""]
+ * @param {string} [options.backgroundColor = "transparent"]
  * 
  * @returns {SVGElement} An SVG element with specified width and height
  */
 export function createSVG(width = 100, height = 100, {
-    units = '',
+    units = "",
     backgroundColor = "transparent"
 } = {}) {
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    svg.setAttribute('width', `${width}${units}`);
-    svg.setAttribute('height', `${height}${units}`);
-    svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
-    svg.setAttribute('style', `background-color:${backgroundColor}`)
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svg.setAttribute("width", `${width}${units}`);
+    svg.setAttribute("height", `${height}${units}`);
+    svg.setAttribute("viewBox", `0 0 ${width} ${height}`);
+    svg.setAttribute("style", `background-color:${backgroundColor}`)
     return svg;
 }
