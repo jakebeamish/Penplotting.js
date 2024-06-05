6ccc5e5edc55101d5e5981b1c7291c3495973db6
Jake Beamish <jakebeamish@gmail.com>
2024-06-05 01:00:14 +0100

    Put the createSVG default options in an object in the same way as the Sketch constructor

--- source/createSVG.js
+++ source/createSVG.js
@@ -12,8 +12,10 @@
  * 
  * @returns {SVGElement} An SVG element with specified width and height
  */
-export function createSVG(width = 100, height = 100, options = {} ) {
-    let { units = '', backgroundColor = 'transparent' } = options
+export function createSVG(width = 100, height = 100, {
+    units = '',
+    backgroundColor = "transparent"
+} = {}) {
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
     svg.setAttribute('width', `${width}${units}`);
