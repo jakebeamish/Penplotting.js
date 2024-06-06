07af4f157cb07025fb37f24cf522a95b8d391a51
Jake Beamish <jakebeamish@gmail.com>
2024-06-06 14:07:47 +0100

    Add some more tests

--- source/tests/unit/Line.test.js
+++ source/tests/unit/Line.test.js
@@ -1,11 +1,12 @@
 import { Line } from "../../Line";
 import { Vector } from "../../Vector";
 
-test('The length method returns the distance between the startpoint and endpoint of the line', () => {
-    let l = new Line(
-        new Vector(0, 0),
-        new Vector(10, 0)
-    );
-
-    expect(l.length()).toBe(10);
-})
+describe('The Line class', () => {
+    test('The length method returns the distance between the startpoint and endpoint of the line', () => {
+        let l = new Line(
+            new Vector(0, 0),
+            new Vector(10, 0)
+        );
+        expect(l.length()).toBe(10);
+    });
+});
--- source/tests/unit/Vector.test.js
+++ source/tests/unit/Vector.test.js
@@ -11,3 +11,41 @@ describe('Vector class', () => {
         expect(v instanceof Vector).toBe(true);
     })
 })
+
+describe('Vector.lerp()', () => {
+    test('It returns a new Vector', () => {
+        const a = new Vector(0, 0);
+        const b = new Vector(1, 1);
+        const amount = 0.5;
+
+        const output = Vector.lerp(a, b, amount);
+
+        expect(output instanceof Vector).toBeTruthy();
+    });
+
+    test("Vector.lerp( [0,0], [1,1], 0.5 ) returns [0.5, 0.5]", () => {
+        const a = new Vector(0, 0);
+        const b = new Vector(1, 1);
+        const amount = 0.5;
+
+        expect(Vector.lerp(a, b, amount)).toMatchObject(new Vector(0.5, 0.5))
+    })
+})
+
+describe("Dot product", () => {
+    test("The dot product of (2,2) and (1,4) should be 10", () => {
+        expect(Vector.dot(
+            new Vector(2, 2),
+            new Vector(1, 4)
+        )).toBe(10)
+    })
+})
+
+describe("Cross product", () => {
+    test("The cross product of (2,2) and (1,4) should be 6", () => {
+        expect(Vector.cross(
+            new Vector(2, 2),
+            new Vector(1, 4)
+        )).toBe(6)
+    })
+})
