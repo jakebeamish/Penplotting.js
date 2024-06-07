4413cb0562f04564a3ec4882b10f31c891994667
Jake Beamish <jakebeamish@gmail.com>
2024-06-07 01:26:49 +0100

    Rewrite wrap() test

--- source/tests/unit/wrap.test.js
+++ source/tests/unit/wrap.test.js
@@ -2,10 +2,13 @@ import { wrap } from "../../utils";
 
 describe('wrap()', () => {
 
-    let x = wrap(-1, 0, 7);
+    test("It returns numbers between min and max", () => {
 
-    test("It returns numbers between mix and max", () => {
-        expect(x).toBeGreaterThanOrEqual(0);
-        expect(x).toBeLessThan(7)
+        const min = 0;
+        const max = 7;
+        let x = wrap(-1, min, max);
+
+        expect(x).toBeGreaterThanOrEqual(min);
+        expect(x).toBeLessThan(max)
     })
 })
