5153e60cd04f85b6dfd411ef4460f05a881827c6
Jake Beamish <jakebeamish@gmail.com>
2024-06-07 12:54:02 +0100

    Add test cases for map()

--- source/tests/unit/map.test.js
+++ source/tests/unit/map.test.js
@@ -17,4 +17,18 @@ describe("map()", () => {
         expect(output).toBeLessThanOrEqual(max2);
         expect(output).toBeGreaterThanOrEqual(min2);
     });
+
+    test("If withinBounds = false, the output should not be constrained by min2 and max2", () => {
+
+
+        let val = 11;
+        let min1 = 0;
+        let max1 = 10;
+        let min2 = 5;
+        let max2 = 6;
+
+        let output = map(val, min1, max1, min2, max2);
+
+        expect(output).toBeGreaterThan(max2);
+    });
 });
