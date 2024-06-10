b7fb5274a8dcab49acfd3d00f0cb483f28acc26a
Jake Beamish <jakebeamish@gmail.com>
2024-06-10 12:58:05 +0100

    Use const when variable isn't changed

--- source/utils.js
+++ source/utils.js
@@ -16,7 +16,7 @@ export class LCG {
 }
 
 export function randomInteger(min, max, randomfn = Math.random()) {
-	let range = max - min;
+	const range = max - min;
 	return min + Math.floor(randomfn * range);
 }
 
