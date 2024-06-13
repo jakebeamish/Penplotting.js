ceff89c5d38aa065937cd6c79e479533bdb7f811
Jake Beamish <jakebeamish@gmail.com>
2024-06-13 01:14:09 +0100

    Delete old moved random functions from utils, as they are now in Random.js

--- source/utils.js
+++ source/utils.js
@@ -1,29 +1,3 @@
-// /**
-//  * @class Linear Congruential Generator
-//  */
-// export class LCG {
-// 	/**
-// 	 * @param {number} [seed=0]
-// 	 */
-// 	constructor(seed = 0) {
-// 		this.seed = seed;
-// 	}
-
-// 	random() {
-// 		this.seed = (this.seed * 1664525 + 1013904223) % 4294967296;
-// 		return this.seed / 4294967296;
-// 	}
-// }
-
-// export function randomInteger(min, max, randomfn = Math.random()) {
-// 	const range = max - min;
-// 	return min + Math.floor(randomfn * range);
-// }
-
-// export function randomElement(array, randomfn = Math.random()) {
-// 	const index = Math.floor(randomfn * array.length);
-// 	return array[index];
-// }
 
 export function sinFract(x, y, a, b, m) {
 	return fract(Math.sin(x * a + y * b) * m);
