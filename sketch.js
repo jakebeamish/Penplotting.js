96d9f7e0ef9b63c8842d9b8769a636fd50e8699d
Jake Beamish <jakebeamish@gmail.com>
2024-06-14 15:32:49 +0100

    Put nearest neighbours demo into main, use sketch.generate()

--- sketch.js
+++ sketch.js
@@ -10,44 +10,46 @@ let sketch = new Sketch(200, 282,
 
 sketch.generate = function () {
 
-    const { width, height } = sketch;
-    const lcg = new LCG(sketch.seed.decimal);
-    const margin = 0.1 * (width + height) * 0.5;
+    let { width, height } = sketch;
+    const centre = new Vector(width / 2, height / 2)
 
-    const options = {
-        cols: [4, 5, 7],
-        rows: [3, 5, 8],
-        offset: [0],
-        lines: [20, 100, 1000]
-    }
+    let margin = width * 0.1;
 
-    const cols = lcg.randomElement(options.cols);
-    const rows = lcg.randomElement(options.rows);
+    let points = [];
 
-    let grid = [];
+    while (points.length < 6000) {
 
-    for (let j = 0; j < rows; j++) {
-        grid[j] = [];
-        for (let i = 0; i < cols; i++) {
-            const x = margin + (((i + 0.5) / cols) * (width - (margin * 2)));
-            const y = margin + (((j + 0.5) / rows) * (height - (margin * 2)));
-            const v = new Vector(x, y);
-            const r = new Vector(
-                (lcg.randomFloat() * 2) - 1,
-                (lcg.randomFloat() * 2) - 1
-            )
-            v.add(r.multiply(lcg.randomElement(options.offset)));
+        let theta = Math.random() * Math.PI * 2;
+        let r = Math.random() * (width - (margin * 2)) * 0.5;
+        let point = new Vector(
+            Math.cos(theta) * r,
+            Math.sin(theta) * r
+        )
 
-            grid[j].push(v);
+        point.add(centre)
+
+        let valid = true;
+        for (let other of points) {
+            if (point.distance(other) < 1.1) {
+                valid = false;
+            }
+        }
+        if (valid) {
+            points.push(point);
         }
     }
 
-    for (let c = 0; c < lcg.randomElement(options.lines); c++) {
-        const a = grid[lcg.randomInteger(0, rows)][lcg.randomInteger(0, cols)];
-        const b = grid[lcg.randomInteger(0, rows)][lcg.randomInteger(0, cols)];
-        const l = new Line(a, b);
-        sketch.lines.push(l)
+    for (let point of points) {
+
+        let k = point.distance(centre) / (width / 2) - Math.random() * 0.2;
+        let nearestNeighbours = point.nearestNeighbour(points, 5 - k * 2);
+        for (let neighbour of nearestNeighbours) {
+            sketch.lines.push(new Line(
+                point, neighbour
+            ))
+        }
     }
+
 }
 
 sketch.draw();
