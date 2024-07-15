ecf86b1dbccd61a21e039b8ba5c1398188a8d175
Jake Beamish <jakebeamish@gmail.com>
2024-07-15 11:45:18 +0100

    Add more blocks of parallel lines to the sketch, reduce random distribution and one-off lines

--- sketches/2024-07-11/sketch.js
+++ sketches/2024-07-11/sketch.js
@@ -1,7 +1,7 @@
 import { Sketch, PAPER, Vector, Line, XORShift32 } from "../../source/index.js";
 
 const sketch = new Sketch({
-    size: PAPER.A5,
+    size: PAPER.A4,
     backgroundColor: "#DDDDDD",
     strokeWeight: 0.1,
     seed: {
@@ -11,7 +11,7 @@ const sketch = new Sketch({
 })
 
 const { width, height } = sketch.size;
-const margin = width * 0.2;
+const margin = width * 0.1;
 const centre = new Vector(width / 2, height / 2)
 const prng = new XORShift32(sketch.seed.decimal)
 
@@ -33,39 +33,39 @@ sketch.generate = () => {
     ]
 
     primaryMoves.forEach((move) => {
-        move.multiply(30)
+        move.multiply(50)
     });
 
     diagonalMoves.forEach((move) => {
         move.y *= 0.5;
-        move.multiply(20)
+        move.multiply(30)
     })
 
     moves = moves.concat(primaryMoves, diagonalMoves);
 
     const start = centre;
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 5; i++) {
         let current = start;
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < 20; i++) {
             let move = prng.randomElement(moves);
 
-            prng.randomChance(0.2) ? move.multiply(0.5): null;
+            prng.randomChance(0.01) ? move.multiply(0.5): null;
 
             let next = Vector.add(current, move);
 
             if (next.x < margin || next.x > width - margin ||
-                next.y < margin || next.y > height - margin
+                next.y < margin || next.y > height - margin * 3
             ) {
                 next.add(move.multiply(-1))
             }
 
-            if (prng.randomChance(0.5)) {
+            if (prng.randomChance(1)) {
                 sketch.lines.push(new Line(current, next));
 
-                if (current.x !== next.x) {
-                    if (prng.randomChance(0.4)) {
-                        let n = prng.randomElement([5, 20, 50, 0]);
+                if (current.x !== next.x && current.y < height-margin*3) {
+                    if (prng.randomChance(1)) {
+                        let n = prng.randomElement([5, 20, 50, 100]);
                         for (let i = 0; i < n; i++) {
                             sketch.lines.push(new Line(
                                 Vector.add(current, new Vector(0, i * 0.5)),
