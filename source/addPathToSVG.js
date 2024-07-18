892c3847cac086e48883ca15cd2cf26d06562859
Jake Beamish <jakebeamish@gmail.com>
2024-07-18 14:51:32 +0100

    Rename path to pathElement, accept path object as argument for addPathToSVG function

--- source/addPathToSVG.js
+++ source/addPathToSVG.js
@@ -1,23 +1,27 @@
-export function addPathToSVG(svg, points, {
+export function addPathToSVG(svg, path, {
     stroke = "black",
     fill = "transparent",
-    strokeWidth = 0.1
+    strokeWidth = 0.1,
 } = {}) {
 
-    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
 
     let commandString = "";
 
-    commandString += `M ${points[0].x} ${points[0].y} `
-    for (let i = 1; i < points.length; i++) {
-        commandString += `L ${points[i].x} ${points[i].y} `
+    commandString += `M ${path.points[0].x} ${path.points[0].y} `
+    for (let i = 1; i < path.points.length; i++) {
+        commandString += `L ${path.points[i].x} ${path.points[i].y} `
     }
 
-    path.setAttribute("d", commandString)
-    path.setAttribute("stroke", stroke);
-    path.setAttribute("stroke-width", strokeWidth);
-    path.setAttribute("fill", fill);
+    if (path.isClosed) {
+        commandString += "Z";
+    }
 
-    svg.appendChild(path);
+    pathElement.setAttribute("d", commandString)
+    pathElement.setAttribute("stroke", stroke);
+    pathElement.setAttribute("stroke-width", strokeWidth);
+    pathElement.setAttribute("fill", fill);
+
+    svg.appendChild(pathElement);
     return svg;
 }
