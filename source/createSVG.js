b40dc28375d818d515f67900dc5a5fc802470727
Jake Beamish <jakebeamish@gmail.com>
2024-07-22 02:05:35 +0100

    docs: tweak createSVG documentation

--- source/createSVG.js
+++ source/createSVG.js
@@ -1,16 +1,16 @@
 /**
- * Create an SVG element with specified width and height
+ * Create an SVG element.
  * @example
  * // returns <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" style="background-color:transparent">
  * createSVG();
  *
- * @param {number} [width=100]
- * @param {number} [height=100]
- * @param {Object} [options]
+ * @param {number} [width=100] - The width of the SVG.
+ * @param {number} [height=100] - The height of the SVG.
+ * @param {Object} [options] - Styling and configuration options.
  * @param {string} [options.units = ""]
- * @param {string} [options.backgroundColor = "transparent"]
+ * @param {string} [options.backgroundColor = "transparent"] - The background colour of the SVG.
  *
- * @returns {SVGElement} An SVG element with specified width and height
+ * @returns {SVGElement} An SVG element with specified size and style.
  */
 export function createSVG(
 	width = 100,
