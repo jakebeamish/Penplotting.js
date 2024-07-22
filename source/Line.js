88430225e6d5c55b1fc3ff59a12b2141c4df9992
Jake Beamish <jakebeamish@gmail.com>
2024-07-22 02:01:11 +0100

    docs: Add documentation to Line class methods

--- source/Line.js
+++ source/Line.js
@@ -1,11 +1,9 @@
-/**
- * @class
- */
+/** Class representing a line. */
 export class Line {
 	/**
-	 *
-	 * @param {Vector} a
-	 * @param {Vector} b
+	 * Create a line between two [Vectors]{@link Vector}.
+	 * @param {Vector} a - The startpoint.
+	 * @param {Vector} b - The endpoint.
 	 */
 	constructor(a, b) {
 		this.a = a;
@@ -17,12 +15,19 @@ export class Line {
 	}
 
 	/**
-	 * @returns {number} The distance between the startpoint and endpoint of the line
+	 * Get the length.
+	 * @returns {number} The distance from the startpoint to the endpoint.
+	 * Uses the {@link Vector#distance} method.
 	 */
 	length() {
 		return this.a.distance(this.b);
 	}
 
+	/**
+	 * Check if this line is the same as another line.
+	 * @param {Line} line - The target line to compare to.
+	 * @returns {boolean} True if startpoints and endpoints are identical or in reverse.
+	 */
 	isDuplicate(line) {
 		return (this.a.equals(line.a) && this.b.equals(line.b)) ||
 			(this.a.equals(line.b) && this.b.equals(line.a));
