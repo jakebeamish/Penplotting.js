f7e45ca9a2940d272d6c5c329a99817055bcd946
Jake Beamish <jakebeamish@gmail.com>
2024-07-22 01:20:40 +0100

    docs: add Circle class

--- source/Circle.js
+++ source/Circle.js
@@ -1,4 +1,11 @@
+/** Class representing a circle. */
 export class Circle {
+    /**
+     * Create a circle.
+     * @param {number} x - The x value of the centre.
+     * @param {number} y - The y value of the centre.
+     * @param {number} radius - The radius.
+     */
     constructor(x, y, radius) {
         this.x = x;
         this.y = y;
