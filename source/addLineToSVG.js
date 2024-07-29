08897fed19bc1d041008a1d3cd20548b282a9d9b
Jake Beamish <jakebeamish@gmail.com>
2024-07-29 11:35:35 +0100

    refactor: use destructuring rather than conditional assignment for addLineToSVG arguments (fixes #18)

--- source/addLineToSVG.js
+++ source/addLineToSVG.js
@@ -11,11 +11,11 @@
  * @param {number} [options.strokeWidth = 0.1]
  * @returns {SVGElement}
  */
-export function addLineToSVG(svg, x1, y1, x2, y2, options) {
-	let units = options?.units || "";
-	let stroke = options?.stroke || "black";
-	let strokeWidth = options?.strokeWidth || 0.1;
-
+export function addLineToSVG(svg, x1, y1, x2, y2, {
+	units = "",
+	stroke = "black",
+	strokeWidth = 0.1
+} = {}) {
 	const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
 	line.setAttribute("x1", `${x1}${units}`);
 	line.setAttribute("y1", `${y1}${units}`);
