aa670c017e2302d61449c4ef1ab6918991bce37e
Jake Beamish <jakebeamish@gmail.com>
2024-08-16 12:45:15 +0100

    refactor line intersection tests to use Line.fromArray for conciseness

--- source/tests/intersectionLineLine.spec.js
+++ source/tests/intersectionLineLine.spec.js
@@ -1,30 +1,23 @@
 import { intersectionLineLine } from "../intersectionLineLine";
 import { Line } from "../Line";
-import { Vector } from "../Vector";
 
 describe("intersectionLineLine", () => {
 
     it("returns false if two parallel lines do not intersect.", () => {
-        const a = new Vector(0, 0);
-        const b = new Vector(5, 0);
-        const c = new Vector(0, 1);
-        const d = new Vector(5, 1);
-        expect(intersectionLineLine(new Line(a, b), new Line(c, d))).toBeFalsy();
+        const line1 = Line.fromArray([0, 0, 5, 0]);
+        const line2 = Line.fromArray([0, 1, 5, 1]);
+        expect(intersectionLineLine(line1, line2)).toBeFalsy();
     });
 
     it("returns false if two non-parallel lines do not intersect.", () => {
-        const a = new Vector(0, 0);
-        const b = new Vector(5, 0);
-        const c = new Vector(0, 1);
-        const d = new Vector(5, 5);
-        expect(intersectionLineLine(new Line(a, b), new Line(c, d))).toBeFalsy();
+        const line1 = Line.fromArray([0, 0, 5, 0]);
+        const line2 = Line.fromArray([0, 1, 5, 5]);
+        expect(intersectionLineLine(line1, line2)).toBeFalsy();
     })
 
     it("returns true if two lines do intersect.", () => {
-        const a = new Vector(0, 0);
-        const b = new Vector(5, 0);
-        const c = new Vector(2, 2);
-        const d = new Vector(2, -2);
-        expect(intersectionLineLine(new Line(a, b), new Line(c, d))).toBeTruthy();
+        const line1 = Line.fromArray([0, 0, 5, 0]);
+        const line2 = Line.fromArray([2, 2, 2, -2]);
+        expect(intersectionLineLine(line1, line2)).toBeTruthy();
     })
 })
