594fe276260c8cc991d21893ceb1664036e988d8
Jake Beamish <jakebeamish@gmail.com>
2024-08-22 01:45:18 +0100

    refactor: create svgbuilder class

---
+++ source/SVGBuilder.js
@@ -0,0 +1,33 @@
+export class SVGBuilder {
+    constructor() {
+        this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    }
+
+    setWidth(width) {
+        this.svg.setAttribute("width", width);
+        return this;
+    }
+
+    setHeight(height) {
+        this.svg.setAttribute("height", height);
+        return this;
+    }
+
+    setViewBox(viewBox) {
+        this.svg.setAttribute("viewBox", viewBox);
+        return this;
+    }
+
+    addShape(shapeElement) {
+        this.svg.appendChild(shapeElement);
+        return this;
+    }
+
+    build() {
+        return this.svg;
+    }
+
+    clear() {
+        this.svg.innerHTML = ''; // Clear all child elements
+    }
+}
