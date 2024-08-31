b6ae553442ce07823beb952cad9b7268d554e573
Jake Beamish <jakebeamish@gmail.com>
2024-09-01 00:43:18 +0100

    chore: move sketch add tests to sketch.spec.js

--- source/tests/Sketch.spec.js
+++ source/tests/Sketch.spec.js
@@ -92,5 +92,63 @@ describe("Sketch", () => {
             expect(mockCircle2.toSVGElement).toHaveBeenCalledWith();
         });
     });
+
+    describe("add", () => {
+    
+        test("Adds a line object to a sketch", () => {
+            const sketch = new Sketch();
+    
+            const line = new Line(
+                new Vector(0, 0),
+                new Vector(5, 5)
+            );
+    
+            sketch.add(line);
+    
+            expect(sketch.lines).toContain(line);
+            expect(sketch.lines.length).toBe(1);
+        })
+    
+        test("Adds an array of lines to a sketch", () => {
+            const sketch = new Sketch();
+    
+            const lines = [
+                new Line(
+                    new Vector(0, 0),
+                    new Vector(5, 5)
+                ),
+                new Line(
+                    new Vector(3, 1),
+                    new Vector(4, 4)
+                )
+            ]
+    
+            sketch.add(lines);
+    
+            expect(sketch.lines).toEqual(lines);
+        })
+    
+        test("Adds a single line and an inner array of lines to a sketch", () => {
+            const sketch = new Sketch();
+    
+            const array = [
+                new Line(
+                    new Vector(0, 0),
+                    new Vector(5, 5)
+                ),
+                new Line(
+                    new Vector(3, 1),
+                    new Vector(4, 4)
+                )
+            ]
+    
+            const line = new Line(
+                new Vector(10, 10),
+                new Vector(11, 17)
+            );
+            sketch.add([array, line]);
+            expect(sketch.lines.length).toBe(3);
+        });
+    });
 });
 
