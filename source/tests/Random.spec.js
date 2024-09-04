a5efb28f3138bee22657e08ed37754114826753c
Jake Beamish <jakebeamish@gmail.com>
2024-09-04 15:35:49 +0100

    test: move LCG tests into Random.spec.js

--- source/tests/Random.spec.js
+++ source/tests/Random.spec.js
@@ -1,9 +1,10 @@
 import { PRNG } from "../Random";
 import { Mulberry32 } from "../Random";
 import { XORShift32 } from "../Random";
+import { LCG } from "../Random";
 
 describe("PRNG", () => {
-    describe("Throws an error if it's methods are called directly.", () => {
+    describe("constructor", () => {
         const prng = new PRNG();
         function maxValue() { prng.maxValue(); }
         function next() { prng.next(); }
@@ -14,6 +15,60 @@ describe("PRNG", () => {
 
         it("Throws an error if next() is called directly.", () => {
             expect(next).toThrow(new Error("Method next() must be implemented."));
+        });
+    });
+
+    describe("next", () => {
+        it("Generates the same sequence of numbers for the same seed.", () => {
+            const seed = 12345;
+            const lcg1 = new LCG(seed);
+            const lcg2 = new LCG(seed);
+    
+            for (let i = 0; i < 5; i++) {
+                expect(lcg1.randomFloat()).toBeCloseTo(lcg2.randomFloat());
+            }
+        });
+
+        it("Generates different sequences for different seeds.", () => {
+            const lcg1 = new LCG(12345);
+            const lcg2 = new LCG(54321);
+    
+            const sequence1 = [];
+            const sequence2 = [];
+    
+            for (let i = 0; i < 5; i++) {
+                sequence1.push(lcg1.randomFloat());
+                sequence2.push(lcg2.randomFloat());
+            }
+    
+            expect(sequence1).not.toEqual(sequence2);
+        });
+    });
+
+    describe("randomInteger", () => {
+		const lcg = new LCG(12345);
+		let x = lcg.randomInteger(0, 10);
+		it("Always returns an integer.", () => {
+			expect(typeof x).toBe("number");
+			expect(x).toBe(parseInt(x));
+		});
+
+		it("Returns integers between min and max.", () => {
+			expect(x).toBeGreaterThanOrEqual(0);
+			expect(x).toBeLessThan(10);
+		});
+	});
+	
+
+    describe("randomFloat", () => {
+        it("It always returns values between 0 and 1.", () => {
+            let lcg = new LCG();
+    
+            for (let i = 0; i < 100; i++) {
+                let value = lcg.randomFloat();
+                expect(value).toBeGreaterThanOrEqual(0);
+                expect(value).toBeLessThan(1);
+            }
         });
     });
 
@@ -50,7 +105,7 @@ describe("PRNG", () => {
         });
     });
 
-    describe("randomBipolarFloat()", () => {
+    describe("randomBipolarFloat", () => {
 
         it("Returns a number between -1 and 1.", () => {
             let prng = new Mulberry32();
