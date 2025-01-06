ee7a27809bc5801b8d585226b6bf73ae05617d68
Jake Beamish <jakebeamish@gmail.com>
2025-01-06 14:21:09 +0000

    test: improve matrix testing

--- source/tests/Matrix.spec.js
+++ source/tests/Matrix.spec.js
@@ -11,12 +11,113 @@ describe("Matrix", () => {
         expect(element instanceof Matrix).toBeTruthy()
       );
     });
-    it("Returns an error if rows and cols are not positive integers.", () => {
-      expect(() => new Matrix(-1, 0)).toThrow();
-      expect(() => new Matrix(0, 6)).toThrow();
-      expect(() => new Matrix(5, -3)).toThrow();
-      expect(() => new Matrix(3.5, 4)).toThrow();
-      expect(() => new Matrix("2", "3")).toThrow();
+    it.each([
+      [-1, 0],
+      [0, 6],
+      [5, -3],
+      [3.5, 4],
+      ["2", "3"],
+    ])(
+      "Returns an error given %p rows and %p cols (not positive integers).",
+      (a, b) => {
+        expect(() => new Matrix(a, b)).toThrow();
+      }
+    );
+  });
+  describe("static methods", () => {
+    describe("fromVector", () => {
+      it.each([
+        [new Vector(1, 2, 3), [[1], [2], [3]]],
+        [new Vector(), [[0], [0], [0]]],
+        [new Vector(9, 3.21), [[9], [3.21], [0]]],
+      ])(
+        "Given valid Vector %p, returns a valid matrix %p.",
+        (vector, expectedMatrix) => {
+          expect(Matrix.fromVector(vector).matrix).toEqual(expectedMatrix);
+        }
+      );
+    });
+
+    describe("hasEqualDimensions", () => {
+      it.each([
+        [new Matrix(1, 2), new Matrix(1, 3)],
+        [new Matrix(4, 4), new Matrix(5, 5)],
+        [new Matrix(1, 6), new Matrix(6, 1)],
+      ])("Returns false given %o and %o.", (a, b) => {
+        expect(Matrix.hasEqualDimensions(a, b)).toBeFalsy();
+      });
+
+      it.each([
+        [new Matrix(1, 1), new Matrix(1, 1)],
+        [new Matrix(8, 5), new Matrix(8, 5)],
+        [new Matrix(3, 1), new Matrix(3, 1)],
+      ])("Returns true given %o and %o.", (a, b) => {
+        expect(Matrix.hasEqualDimensions(a, b)).toBeTruthy();
+      });
+    });
+    describe("add", () => {
+      it.each([
+        [
+          [
+            [1, 2],
+            [3, 4],
+          ],
+          [
+            [4, 3],
+            [2, 1],
+          ],
+          [
+            [5, 5],
+            [5, 5],
+          ],
+        ],
+        [
+          [
+            [0, -1],
+            [0, 0],
+          ],
+          [
+            [-1, -1],
+            [1, 0],
+          ],
+          [
+            [-1, -2],
+            [1, 0],
+          ],
+        ],
+      ])(
+        "Creates a new matrix which is the result of matrix addition.",
+        (a, b, expected) => {
+          const m = new Matrix(2, 2);
+          const n = new Matrix(2, 2);
+          m.matrix = a;
+          n.matrix = b;
+          const result = Matrix.add(m, n);
+          expect(result.matrix).toEqual(expected);
+        }
+      );
+
+      it.each([
+				["m", [3, 2]],
+				[[1, 2, 3], new Vector(1, 2, 3)],
+				[1, 5],
+				[new Matrix(1, 1), 1],
+				[{
+					matrix: [
+						[1, 2, 3]
+					]
+				}, new Matrix(1, 3)]
+			])("Throws an error given inputs %p and %p (not matrices).", (a, b) => {
+        expect(() => Matrix.add(a, b)).toThrow();
+      });
+
+      it("Throws an error if input matrices do not have equal dimensions.", () => {
+        const m = new Matrix(1, 2);
+        const n = new Matrix(3, 4);
+        expect(() => {
+          Matrix.add(m, n);
+        }).toThrow();
+      });
     });
   });
 
@@ -26,14 +127,6 @@ describe("Matrix", () => {
       m.matrix = [[1, 2]];
       const vector = m.toVector();
       expect(vector).toEqual(Vector.fromArray([1, 2]));
-    });
-  });
-
-  describe("hasEqualDimensions", () => {
-    it("Compares rows and columns of two matrices.", () => {
-      expect(
-        Matrix.hasEqualDimensions(new Matrix(3, 2), new Matrix(3, 2))
-      ).toBeTruthy();
     });
   });
 
@@ -84,59 +177,6 @@ describe("Matrix", () => {
     });
   });
 
-  describe("static add", () => {
-    it("Creates a new matrix which is the result of matrix addition.", () => {
-      const m = new Matrix(2, 2);
-      const n = new Matrix(2, 2);
-      m.matrix = [
-        [1, 2],
-        [3, 4],
-      ];
-
-      n.matrix = [
-        [4, 3],
-        [2, 1],
-      ];
-
-      const result = Matrix.add(m, n);
-
-      expect(result.matrix).toEqual([
-        [5, 5],
-        [5, 5],
-      ]);
-    });
-
-    it("Throws an error if inputs are not matrices.", () => {
-      expect(() => Matrix.add("m", [3, 2])).toThrow();
-      expect(() =>
-        Matrix.add(
-          [
-            [1, 2],
-            [3, 4],
-          ],
-          new Vector(1, 1)
-        )
-      ).toThrow();
-    });
-
-    it("Throws an error if input matrices do not have equal dimensions.", () => {
-      const m = new Matrix(1, 2);
-      const n = new Matrix(3, 4);
-      expect(() => {
-        Matrix.add(m, n);
-      }).toThrow();
-    });
-  });
-
-  describe("fromVector", () => {
-    it("Returns a valid matrix from Vector components.", () => {
-      const v = new Vector(1, 2, 3);
-      const expected = [[1], [2], [3]];
-
-      expect(Matrix.fromVector(v).matrix).toEqual(expected);
-    });
-  });
-
   describe("multiply", () => {
     it("Throws an error if inputs are not matrices.", () => {
       expect(() => {
@@ -148,31 +188,23 @@ describe("Matrix", () => {
     });
 
     it("Throws an error if columns of the first input don't match rows of the second.", () => {
-			const m = new Matrix(1, 1);
-			const n = new Matrix(3, 1);
-			expect(() => {
-				Matrix.multiply(m, n)
-			}).toThrow();
-		});
+      const m = new Matrix(1, 1);
+      const n = new Matrix(3, 1);
+      expect(() => {
+        Matrix.multiply(m, n);
+      }).toThrow();
+    });
 
-		it("Returns a new matrix if given two matrices of correct size.", () =>  {
-			const m = new Matrix(2, 3);
-			const n = new Matrix(3, 1);
-			m.matrix = [
-				[1, 0, 0],
-				[0, 1, 0]
-			];
-			n.matrix = [
-				[5],
-				[4],
-				[2]
-			];
-			expect(Matrix.multiply(m, n).matrix).toEqual([
-				[5],
-				[4],
-				[0]
-			]);
-		})
+    it("Returns a new matrix if given two matrices of correct size.", () => {
+      const m = new Matrix(2, 3);
+      const n = new Matrix(3, 1);
+      m.matrix = [
+        [1, 0, 0],
+        [0, 1, 0],
+      ];
+      n.matrix = [[5], [4], [2]];
+      expect(Matrix.multiply(m, n).matrix).toEqual([[5], [4], [0]]);
+    });
   });
 
   describe("scale", () => {
