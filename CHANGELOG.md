db26ec8b9d55311b41cd4e153bc4f4cb6ff413ed
Jake Beamish <jakebeamish@gmail.com>
2025-01-21 11:35:09 +0000

    feat: circle from vector static constructor (closes #83)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - Vector distance squared static method.
+- Circle from vector static constructor method.
 
 ## [5.2.0] - 2025-01-20
 
--- source/tests/Circle.spec.js
+++ source/tests/Circle.spec.js
@@ -1,10 +1,24 @@
 import { Circle } from "../Circle";
+import { Vector } from "../Vector";
 
 describe("Circle", () => {
   describe("constructor", () => {
     it("Returns a valid circle object if arguments are numbers.", () => {
       const circle = new Circle(0, 0, 10);
       expect(circle instanceof Circle).toBeTruthy();
+    });
+  });
+
+  describe("fromVector", () => {
+    it("Returns a valid circle object if given a Vector and a radius number.", () => {
+      const centre = new Vector();
+      const radius = 5;
+      const circle = new Circle(0, 0, 5);
+      const result = Circle.fromVector(centre, radius);
+
+      expect(result.x).toEqual(circle.x);
+      expect(result.y).toEqual(circle.y);
+      expect(result.radius).toEqual(circle.radius);
     });
   });
 
