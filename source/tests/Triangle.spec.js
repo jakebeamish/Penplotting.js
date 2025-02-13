80bc4cf1c02c058f16cf53a95ef0baf4f4d3adb8
Jake Beamish <jakebeamish@gmail.com>
2025-02-13 16:35:07 +0000

    test(triangle): refactor to use extracted unit right-angled triangle

--- source/tests/Triangle.spec.js
+++ source/tests/Triangle.spec.js
@@ -3,52 +3,48 @@ import { Line } from "../Line";
 import { Vector } from "../Vector";
 
 describe("Triangle", () => {
+
+  /**
+   * @type {Triangle}
+   * A unit right-angled triangle with vertices at (0, 0), (1, 0) and (0, 1)
+   */
+  let triangle;
+
+  beforeEach(() => {
+    const a = new Vector(0, 0);
+    const b = new Vector(1, 0);
+    const c = new Vector(0, 1);
+    triangle = new Triangle(a, b, c);
+  });
+
   describe("constructor", () => {
     it("Returns a valid triangle object if arguments are numbers.", () => {
-      const a = new Vector(0, 0);
-      const b = new Vector(5, 0);
-      const c = new Vector(5, 5);
-      const triangle = new Triangle(a, b, c);
       expect(triangle instanceof Triangle).toBeTruthy();
     });
   });
 
   describe("lines", () => {
     test("Returns an array of line elements.", () => {
-      const a = new Vector(0, 0);
-      const b = new Vector(5, 0);
-      const c = new Vector(5, 5);
-      const triangle = new Triangle(a, b, c);
       expect(triangle.lines()).toEqual([
-        new Line(a, b),
-        new Line(b, c),
-        new Line(c, a),
+        new Line(triangle.a, triangle.b),
+        new Line(triangle.b, triangle.c),
+        new Line(triangle.c, triangle.a)
       ]);
     });
   });
 
   describe("getCentroid", () => {
     it("Returns a vector at the centroid of the triangle.", () => {
-      const triangle = new Triangle(
-        new Vector(0, 0),
-        new Vector(0, 3),
-        new Vector(3, 0),
-      );
-      expect(triangle.getCentroid()).toEqual(new Vector(1, 1));
+      expect(triangle.getCentroid()).toEqual(new Vector(1/3, 1/3));
     });
   });
 
   describe("getMidpoints", () => {
     it("Returns an array of vectors at midpoints of the triangle edges.", () => {
-      const triangle = new Triangle(
-        new Vector(0, 0),
-        new Vector(0, 2),
-        new Vector(2, 0),
-      );
       expect(triangle.getMidpoints()).toEqual([
-        new Vector(0, 1),
-        new Vector(1, 1),
-        new Vector(1, 0),
+        new Vector(0.5, 0),
+        new Vector(0.5, 0.5),
+        new Vector(0, 0.5),
       ]);
     });
   });
